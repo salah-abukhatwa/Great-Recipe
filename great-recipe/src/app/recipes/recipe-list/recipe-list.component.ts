4ffa144e46d77fc57c640d76fec42919c645aa94
salah-abukhatwa <salah.abukhatwa@gmail.com>
2023-05-12 22:32:04 +0300

    Create the structure of all sections

--- great-recipe/src/app/recipes/recipe-list/recipe-list.component.ts
+++ great-recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','this is simply Description','https://www.summahealth.org/-/media/project/summahealth/website/page-content/flourish/2_18a_fl_fastfood_400x400.webp?la=en&h=400&w=400&hash=145DC0CF6234A159261389F18A36742A'),
+    new Recipe('A Test Recipe','this is simply Description','https://www.summahealth.org/-/media/project/summahealth/website/page-content/flourish/2_18a_fl_fastfood_400x400.webp?la=en&h=400&w=400&hash=145DC0CF6234A159261389F18A36742A'),
+
+  ];
   constructor() { }
 
   ngOnInit(): void {
