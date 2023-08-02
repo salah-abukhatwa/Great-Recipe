0ac23e85d1efd01e9655d78426fa1377b6c058e5
salah-abukhatwa <salah.abukhatwa@gmail.com>
2023-08-02 14:31:04 +0300

    Using Environment Variables

--- great-recipe/src/app/auth/auth/auth.service.ts
+++ great-recipe/src/app/auth/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Subject, catchError, tap, throwError } from 'rxjs';
 import { User } from '../user.model';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 export interface AuthResponseData {
   idToken: string;
@@ -41,7 +42,7 @@ export class AuthService {
   }
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDvp6cNlLiDxYcjmNURuPooMi04_Q-_O1M',
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
       {
       email: email,
       password: password,
@@ -55,7 +56,7 @@ export class AuthService {
   }
 
   login(email: string, password: string) {
-  return  this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDvp6cNlLiDxYcjmNURuPooMi04_Q-_O1M',
+  return  this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + environment.firebaseAPIKey,
       {
       email: email,
       password: password,
